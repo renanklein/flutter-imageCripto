3e0ecdf957e01100fc92ade8e1fb6109cd1c7ffa
renanklein <renankleinshow@gmail.com>
2020-08-25 22:18:03 -0300

    Updates dependencies

--- lib/utils/cripto_image.dart
+++ lib/utils/cripto_image.dart
@@ -1,20 +1,18 @@
 import 'dart:typed_data';
 import 'package:encrypt/encrypt.dart';
+
 class CriptoImage {
-
   static final _initVector = IV.fromSecureRandom(16);
-  static Encrypted encrypt(List<int> imageBytes, Key encriptKey){
-
-    var encrypt = Encrypter(new AES(encriptKey));
+  static Encrypted encrypt(List<int> imageBytes, Key encriptKey) {
+    var encrypt = Encrypter(new AES(encriptKey, padding: null));
 
     return encrypt.encryptBytes(imageBytes, iv: _initVector);
   }
 
-  static Uint8List decrypt(Encrypted encryptedImage, Key decriptKey){
+  static Uint8List decrypt(Encrypted encryptedImage, Key decriptKey) {
+    final decripter = Encrypter(new AES(decriptKey, padding: null));
 
-    final decripter = Encrypter(new AES(decriptKey));
-
-    return Uint8List.fromList(decripter.decryptBytes(encryptedImage, iv: _initVector));
-
+    return Uint8List.fromList(
+        decripter.decryptBytes(encryptedImage, iv: _initVector));
   }
 }
